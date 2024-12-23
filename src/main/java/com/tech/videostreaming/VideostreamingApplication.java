115cc2fdbee860c7de38a84cc0653425ee9e3611
Vinay Prakash Yadav <vinayprakashyadav8364@gmail.com>
2024-12-23 16:22:42 +0530

    fix: resolve issue with logger initialization

--- src/main/java/com/tech/videostreaming/VideostreamingApplication.java
+++ src/main/java/com/tech/videostreaming/VideostreamingApplication.java
@@ -1,14 +1,18 @@
 package com.tech.videostreaming;
 
-import lombok.extern.slf4j.Slf4j;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.time.LocalDateTime;
 
-@Slf4j
+
 @SpringBootApplication
 public class VideostreamingApplication {
+	private static final Logger log = LoggerFactory.getLogger(VideostreamingApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(VideostreamingApplication.class, args);
