5be3e481dfe3574cc284c155a18dbd94b2572203
Vinay Prakash Yadav <vinayprakashyadav8364@gmail.com>
2024-12-23 15:34:31 +0530

    added app start time log

--- src/main/java/com/tech/videostreaming/VideostreamingApplication.java
+++ src/main/java/com/tech/videostreaming/VideostreamingApplication.java
@@ -1,13 +1,18 @@
 package com.tech.videostreaming;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDateTime;
+
+@Slf4j
 @SpringBootApplication
 public class VideostreamingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VideostreamingApplication.class, args);
+		log.info("Video Streaming application started successfully at [{}]", LocalDateTime.now());
 	}
 
 }
