08b31f36e884e04e525a8f4786637da8483ce195
Vinay Prakash Yadav <vinayprakashyadav8364@gmail.com>
2024-12-23 15:27:20 +0530

    Initial commit

---
+++ src/main/java/com/tech/videostreaming/VideostreamingApplication.java
@@ -0,0 +1,13 @@
+package com.tech.videostreaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideostreamingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideostreamingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tech/videostreaming/VideostreamingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.videostreaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideostreamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
